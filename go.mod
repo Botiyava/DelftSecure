4619f71232b5f1c0465b5cdc56ce0c68e18b33aa
Botiyava <botiyava.prog@mail.ru>
2022-02-17 15:06:00 +0000

    added connection to TG bot

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module telegramBot
 go 1.17
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/viper v1.10.1 // indirect
--- main.go
+++ main.go
@@ -2,34 +2,39 @@ package main
 
 import (
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"os"
 )
 
 func main() {
-
+	//Logging system
 	logrus.SetFormatter(new(logrus.JSONFormatter))
+
 	file, err := os.OpenFile("log/errors.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
-	//logger.SetOutput(file)
 	logrus.SetOutput(file)
 	if err := initConfig(); err != nil {
 		logrus.Fatal("Error initializing configs: ", err.Error())
 	}
 
-	a := viper.GetString("botSettings.token")
-	fmt.Println(a)
-	fmt.Println("lel")
-
+	//Telegram connection
+	botToken := viper.GetString("botSettings.token")
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil{
+		logrus.Fatal("Error with bot authorization:", err.Error())
+	}
+	fmt.Printf("Successfully authorized on [ %s ]\n", bot.Self.UserName)
+	//bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID))
 
 }
 
 func initConfig() error {
 	viper.AddConfigPath("configs")
-	viper.SetConfigName("confkig")
+	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
